4299b59c6eb95a7bd358f2296ed01725d8d8706c
Dejan Trajkovic <dtrajko@gmail.com>
2021-02-09 03:38:10 +0100

    26. How to make a Third Person Game - Camera & Movements (part 3)

--- DirectXGame/DirectionalLightPixelShader.hlsl
+++ DirectXGame/DirectionalLightPixelShader.hlsl
@@ -1,13 +1,22 @@
-Texture2D TextureColor: register(t0);
-sampler TextureColorSampler: register(s0);
+Texture2D EarthColor: register(t0);
+sampler EarthColorSampler: register(s0);
+
+Texture2D EarthSpecular: register(t1);
+sampler EarthSpecularSampler: register(s1);
+
+Texture2D Clouds: register(t2);
+sampler CloudsSampler: register(s2);
+
+Texture2D EarthNight: register(t3);
+sampler EarthNightSampler: register(s3);
 
 
 struct PS_INPUT
 {
 	float4 position: SV_POSITION;
 	float2 texcoord: TEXCOORD0;
-	float3 normal: NORMAL0;
-	float3 world_pos: TEXCOORD1;
+	float3 normal:   NORMAL0;
+	float3 direction_to_camera: DIRECTIONTOCAMERA0;
 };
 
 cbuffer constant: register(b0)
@@ -17,53 +26,49 @@ cbuffer constant: register(b0)
 	row_major float4x4 m_proj;
 	float4 m_light_direction;
 	float4 m_camera_position;
-	float4 m_light_position;
-	float m_light_radius;
 	float m_time;
 };
 
 
 float4 psmain(PS_INPUT input) : SV_TARGET
 {
-	float4 tex_color = TextureColor.Sample(TextureColorSampler, float2(input.texcoord.x, 1.0 - input.texcoord.y));
+	float4 earth_color = EarthColor.Sample(EarthColorSampler, 1.0 - input.texcoord);
+	float earth_spec = EarthSpecular.Sample(EarthSpecularSampler, 1.0 - input.texcoord).r;
+	float clouds = Clouds.Sample(CloudsSampler, 1.0 - input.texcoord + float2(m_time / 100.0, 0.0)).r;
+	float4 earth_night = EarthNight.Sample(EarthNightSampler, 1.0 - input.texcoord);
 
 	// Ambient light
-	float ka = 0.6;
-	float3 ia = float3(1.0, 1.0, 1.0);
-	ia *= (tex_color.rgb);
+	float ka = 1.5;
+	float3 ia = float3(0.09, 0.082, 0.082);
+	ia *= (earth_color.rgb);
 
 	float3 ambient_light = ka * ia;
 
 	// Diffuse light
 	float kd = 0.7;
-	float3 light_dir = normalize(m_light_position.xyz - input.world_pos.xyz);
-	float distance_light_object = length(m_light_position.xyz - input.world_pos.xyz);
 
-	float fade_area = max(0, distance_light_object - m_light_radius);
+	float id_day = float3(1.0, 1.0, 1.0);
+	id_day *= (earth_color.rgb + clouds);
 
-	float constant_func = 1.0;
-	float linear_func = 2.0;
-	float quadratic_func = 2.0;
+	float id_night = float3(1.0, 1.0, 1.0);
+	id_night *= (earth_night.rgb + clouds * 0.3);
 
-	float attenuation = constant_func + linear_func * fade_area + quadratic_func * fade_area * fade_area;
+	float amount_diffuse_light = dot(m_light_direction.xyz, input.normal);
 
-	float amount_diffuse_light = max(0.0, dot(light_dir, input.normal));
-	float id = float3(1.0, 1.0, 1.0);
-	id *= (tex_color.rgb);
+	float3 id = lerp(id_night, id_day, (amount_diffuse_light + 1.0) / 2.0);
 
-	float3 diffuse_light = (kd * id * amount_diffuse_light) / attenuation;
+	float3 diffuse_light = kd * id;
 
 	// Specular light
-	float ks = 0.0;
-	float3 direction_to_camera = normalize(input.world_pos.xyz - m_camera_position.xyz);
+	float ks = earth_spec;
 	float is = float3(1.0, 1.0, 1.0);
-	float3 reflected_light = reflect(light_dir, input.normal);
+	float3 reflected_light = reflect(m_light_direction.xyz, input.normal);
 	float shininess = 30.0;
-	float amount_specular_light = pow(max(0.0, dot(reflected_light, direction_to_camera)), shininess);
-	float3 specular_light = (ks * amount_specular_light * is) / attenuation;
+	float amount_specular_light = pow(max(0.0, dot(reflected_light, input.direction_to_camera)), shininess);
+	float3 specular_light = ks * amount_specular_light * is;
 
 	// Final light
 	float3 final_light = ambient_light + diffuse_light + specular_light;
 
-	return float4(final_light, 1.0);
+	return float4(final_light, 1.0) * earth_color;
 }
--- DirectXGame/DirectionalLightVertexShader.hlsl
+++ DirectXGame/DirectionalLightVertexShader.hlsl
@@ -9,8 +9,8 @@ struct VS_OUTPUT
 {
 	float4 position: SV_POSITION;
 	float2 texcoord: TEXCOORD0;
-	float3 normal: NORMAL0;
-	float3 world_pos: TEXCOORD1;
+	float3 normal:   NORMAL0;
+	float3 direction_to_camera: DIRECTIONTOCAMERA0;
 };
 
 cbuffer constant: register(b0)
@@ -20,8 +20,6 @@ cbuffer constant: register(b0)
 	row_major float4x4 m_proj;
 	float4 m_light_direction;
 	float4 m_camera_position;
-	float4 m_light_position;
-	float m_light_radius;
 	float m_time;
 };
 
@@ -32,7 +30,7 @@ VS_OUTPUT vsmain(VS_INPUT input)
 
 	// World space
 	output.position = mul(input.position, m_world);
-	output.world_pos = output.position.xyz;
+	output.direction_to_camera = normalize(output.position.xyz - m_camera_position.xyz);
 
 	// View space
 	output.position = mul(output.position, m_view);
