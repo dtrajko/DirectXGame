b2106ffb0e47fa640bf2c76ff49c3a21a408c06b
Dejan Trajkovic <dtrajko@gmail.com>
2021-02-09 03:40:24 +0100

    26. How to make a Third Person Game - Camera & Movements (part 4)

--- DirectXGame/DirectionalLightVertexShader.hlsl
+++ DirectXGame/DirectionalLightVertexShader.hlsl
@@ -20,6 +20,8 @@ cbuffer constant: register(b0)
 	row_major float4x4 m_proj;
 	float4 m_light_direction;
 	float4 m_camera_position;
+	float4 m_light_position;
+	float m_light_radius;
 	float m_time;
 };
 
